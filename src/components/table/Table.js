1a519a492bb325197d91698077bb8a794805a7bb
Alexey Shchcendrygin <specmashalexeysh@gmail.com>
2023-05-09 19:29:27 +0300

    Add resize logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,25 @@ import {createTable} from '@/components/table/table.template'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+    })
+  }
   toHTML() {
     return createTable(20)
+  }
+
+  onClick() {
+  }
+
+  onMousedown(event) {
+  }
+
+  onMousemove() {
+  }
+
+  onMouseup() {
   }
 }

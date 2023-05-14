0fdaf6ca5cabc3eaa69ef90604e178e499be86e5
Alexey Shchcendrygin <specmashalexeysh@gmail.com>
2023-05-14 23:59:21 +0300

    refactoring

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -23,18 +23,29 @@ export class Table extends ExcelComponent {
       const coords = $parent.getCoords()
       let delta = 0
       let value = 0
-      console.log($resizer)
+      const elType = $resizer.data.resize
 
       document.onmousemove = e => {
-        delta = e.pageX - coords.right
-        value = coords.width + delta
-        $resizer.$el.style.left += value
+        if (elType === 'col') {
+          delta = e.pageX - coords.right
+          value = coords.width + delta
+        } else {
+          delta = e.pageY - coords.bottom
+          value = coords.height + delta
+        }
       }
 
       document.onmouseup = () => {
         document.onmousemove = null
-        /* $resizer.$el.classList.remove('resizer-selected')*/
-        $parent.$el.style.width = value + 'px'
+        if (elType === 'col') {
+          this.$root.findAll(`[data-col="${ $parent.data.col }"]`)
+              .forEach(el => {
+                el.style.width = value + 'px'
+              })
+        } else {
+          $parent.css({height: value + 'px'})
+        }
+        $resizer.$el.classList.remove('resizer-selected')
       }
     }
   }

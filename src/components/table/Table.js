30b9d9b32f7a6b48b9b68621fd892b84a0745bc7
Alexey Shchcendrygin <specmashalexeysh@gmail.com>
2023-05-14 19:26:48 +0300

    refactoring

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,30 +1,41 @@
-import { ExcelComponent } from "@core/ExcelComponent";
-import { createTable } from "@/components/table/table.template";
-import { $ } from "../../core/Dom";
+import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from '@/components/table/table.template'
+import {$} from '../../core/Dom'
 
 export class Table extends ExcelComponent {
-  static className = "excel__table";
+  static className = 'excel__table'
 
   constructor($root) {
     super($root, {
-      listeners: ["click", "mousedown", "mousemove", "mouseup"],
-    });
+      listeners: ['mousedown'],
+    })
   }
 
   toHTML() {
-    return createTable(20);
+    return createTable(20)
   }
-
-  onClick() {}
 
   onMousedown(event) {
-    const $resizer = $(event.target);
-    const $parent = $resizer.closest('[data-type="resizable"]');
-    console.log($parent.getCoords());
-    $parent.getCoords().width = $parent.getCoords().width * 2;
+    if (event.target.dataset.resize && event.buttons === 1) {
+      const $resizer = $(event.target)
+      $resizer.$el.classList.add('resizer-selected')
+      const $parent = $resizer.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+      let delta = 0
+      let value = 0
+      console.log($resizer)
+
+      document.onmousemove = e => {
+        delta = e.pageX - coords.right
+        value = coords.width + delta
+        $resizer.$el.style.left += value
+      }
+
+      document.onmouseup = () => {
+        document.onmousemove = null
+        /* $resizer.$el.classList.remove('resizer-selected')*/
+        $parent.$el.style.width = value + 'px'
+      }
+    }
   }
-
-  onMousemove(event) {}
-
-  onMouseup() {}
 }

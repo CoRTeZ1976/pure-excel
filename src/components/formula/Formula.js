b35a5849fe05f9899fef03a80e37e06b615349e3
Alex <specmashalexeysh@gmail.com>
2023-05-17 14:28:27 +0300

    refactoring emitter

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,23 +1,23 @@
-import {ExcelComponent} from '@core/ExcelComponent'
+import { ExcelComponent } from "@core/ExcelComponent";
 
 export class Formula extends ExcelComponent {
-  static className = 'excel__formula'
+  static className = "excel__formula";
   constructor($root, option) {
     super($root, {
-      name: 'Formula',
-      listeners: ['input'],
+      name: "Formula",
+      listeners: ["input"],
       ...option,
-    })
+    });
   }
   toHTML() {
     return `
       <div class="info">fx</div>
             <div class="input" contenteditable spellcheck="false"></div>
-    `
+    `;
   }
 
   onInput(event) {
-    const text = event.target.textContent.trim()
-    this.emitter.emit('Formula input', text)
+    const text = event.target.textContent.trim();
+    this.$emit("Formula:input", text);
   }
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,76 +1,77 @@
-import {ExcelComponent} from '@core/ExcelComponent'
-import {createTable} from '@/components/table/table.template'
-import {resizeHandler} from '@/components/table/table.resize'
+import { ExcelComponent } from "@core/ExcelComponent";
+import { createTable } from "@/components/table/table.template";
+import { resizeHandler } from "@/components/table/table.resize";
 import {
   isCell,
   matrix,
   nextSelector,
   shouldResize,
-} from '@/components/table/table.function'
-import {TableSelection} from '@/components/table/TableSelection'
-import {$} from '@core/Dom'
+} from "@/components/table/table.function";
+import { TableSelection } from "@/components/table/TableSelection";
+import { $ } from "@core/Dom";
 
 export class Table extends ExcelComponent {
-  static className = 'excel__table'
+  static className = "excel__table";
 
   constructor($root, option) {
     super($root, {
-      name: 'Table',
-      listeners: ['mousedown', 'keydown'],
+      name: "Table",
+      listeners: ["mousedown", "keydown"],
       ...option,
-    })
+    });
   }
 
   toHTML() {
-    return createTable()
+    return createTable();
   }
 
   prepare() {
-    this.selection = new TableSelection()
+    this.selection = new TableSelection();
   }
 
   init() {
-    super.init()
-    const $cell = this.$root.find('[data-id="0:0"]')
-    this.selection.select($cell)
+    super.init();
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selection.select($cell);
 
-    this.emitter.subscribe('Formula input', text => {
-      this.selection.current.text(text)
-    })
+    this.$on("Formula:input", (text) => {
+      this.selection.current.text(text);
+    });
   }
 
   onMousedown(event) {
     if (shouldResize(event)) {
-      resizeHandler(this.$root, event)
+      resizeHandler(this.$root, event);
     } else if (isCell(event)) {
-      const $target = $(event.target)
+      const $target = $(event.target);
       if (event.shiftKey) {
-        const $cells = matrix($target, this.selection.current)
-          .map(id => this.$root.find(`[data-id="${ id }"`))
-        this.selection.selectGroup($cells)
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"`)
+        );
+        this.selection.selectGroup($cells);
       } else {
-        this.selection.select($target)
+        this.selection.select($target);
       }
     }
   }
 
   onKeydown(event) {
     const keys = [
-      'Enter',
-      'Tab',
-      'ArrowLeft',
-      'ArrowRight',
-      'ArrowDown',
-      'ArrowUp',
-    ]
+      "Enter",
+      "Tab",
+      "ArrowLeft",
+      "ArrowRight",
+      "ArrowDown",
+      "ArrowUp",
+    ];
 
-    const {key} = event
+    const { key } = event;
 
     if (keys.includes(key) && !event.shiftKey) {
-      event.preventDefault()
-      const id = this.selection.current.id(true)
-      const $next = this.$root.find(nextSelector(key, id))
-      this.selection.select($next)
+      event.preventDefault();
+      const id = this.selection.current.id(true);
+      const $next = this.$root.find(nextSelector(key, id));
+      this.selection.select($next);
     }
   }
 }
